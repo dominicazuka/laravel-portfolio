50e903dd0c541a5c797794554b29f578f6650a61
dominicazuka <visitdominicazuka@gmail.com>
2023-09-28 09:04:14 +0100

    final-portfolio update

--- routes/web.php
+++ routes/web.php
@@ -52,7 +52,6 @@ Route::middleware('auth')->group(function () {
     Route::controller(AboutController::class)->group(function () {
         Route::get('/about/page', 'AboutPage')->name('about.page');
         Route::post('/update/about', 'UpdateAbout')->name('update.about');
-        Route::get('/about', 'HomeAbout')->name('home.about');
         Route::get('/about/multi', 'AboutMultiImage')->name('about.multi.image');
         Route::post('/store/multi/image', 'StoreMultiImage')->name('store.multi.image');
         Route::get('/all/multi/image', 'AllMultiImage')->name('all.multi.image');
@@ -69,8 +68,11 @@ Route::middleware('auth')->group(function () {
         Route::get('/edit/portfolio/{id}', 'EditPortfolio')->name('edit.portfolio');
         Route::post('/update/portfolio', 'UpdatePortfolio')->name('update.portfolio');
         Route::get('/delete/portfolio/{id}', 'DeletePortfolio')->name('delete.portfolio');
-        Route::get('/portfolio/details/{id}', 'PortfolioDetails')->name('portfolio.details');
     });
 });
+
+// Routes accessible to all users (excluding auth middleware)
+Route::get('/about', [AboutController::class, 'homeAbout'])->name('home.about'); // User view
+Route::get('/portfolio/details/{id}', [PortfolioController::class, 'portfolioDetails'])->name('portfolio.details'); // User view
 
 require __DIR__ . '/auth.php';
