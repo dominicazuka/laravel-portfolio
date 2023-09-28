6515a610bd68b259c1715c84e8b71391edc4e250
dominicazuka <visitdominicazuka@gmail.com>
2023-09-28 19:19:06 +0100

    final-portfolio update

--- app/Http/Controllers/Home/BlogController.php
+++ app/Http/Controllers/Home/BlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use App\Models\Blog;
+use App\Models\BlogCategory;
 use Image;
 use Illuminate\Support\Carbon;
 use Illuminate\Http\Request;
@@ -18,6 +19,118 @@ class BlogController extends Controller
 
     public function AddBlog()
     {
-        return view('admin.blogs.blogs_add');
+        $categories = BlogCategory::orderBy('blog_category', 'ASC')->get(); //get data from blog Category model by name in ascending order
+        return view('admin.blogs.blogs_add', compact('categories'));
+    } //end method
+
+    public function StoreBlog(Request $request)
+    {
+        $request->validate([
+            'blog_category_id' => 'required',
+            'blog_title' => 'required',
+            'blog_tags' => 'required',
+            'blog_description' => 'required',
+            'blog_image' => 'required',
+        ], [
+            'blog_category_id.required' => 'Blog name is required',
+            'blog_title.required' => 'Blog title is required',
+            'blog_tags.required' => 'Blog tag is required',
+            'blog_description.required' => 'Blog description is required',
+            'blog_image.required' => 'Blog image is required',
+        ]);
+        $image = $request->file('blog_image');
+        $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension(); //generate random name
+        Image::make($image)->resize(430, 327)->save('upload/blog/' . $name_gen);
+        $save_url = 'upload/blog/' . $name_gen;
+        Blog::insert([
+            'blog_category_id' => $request->blog_category_id,
+            'blog_title' => $request->blog_title,
+            'blog_tags' => $request->blog_tags,
+            'blog_description' => $request->blog_description,
+            'blog_image' => $save_url,
+            'created_at' => Carbon::now(),
+        ]);
+        $notification = array(
+            'message' => 'Blog Post Data Created Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->route('all.blog')->with($notification);
+    } //end method
+
+    public function EditBlog($id)
+    {
+        $blog = Blog::findOrFail($id);
+        $category = BlogCategory::orderBy('blog_category', 'ASC')->get(); //get data from blog Category model by name in ascending order
+        return view('admin.blogs.blogs_edit', compact('blog', 'category'));
+    } //end method
+
+    public function UpdateBlog(Request $request)
+    {
+        $request->validate([
+            'blog_category_id' => 'required',
+            'blog_title' => 'required',
+            'blog_tags' => 'required',
+            'blog_description' => 'required',
+        ], [
+            'blog_category_id.required' => 'Blog name is required',
+            'blog_title.required' => 'Blog title is required',
+            'blog_tags.required' => 'Blog tag is required',
+            'blog_description.required' => 'Blog description is required',
+        ]);
+        $blog = Blog::findOrFail($request->id); //retrieve existing blog data
+        if ($request->file('blog_image')) {
+            $image = $request->file('blog_image');
+            $name_gen = hexdec(uniqid()) . '.' . $image->getClientOriginalExtension();
+            Image::make($image)->resize(430, 327)->save('upload/blog/' . $name_gen);
+            $save_url = 'upload/blog/' . $name_gen;
+
+            //delete the existing image on localhost
+            if (file_exists($blog->blog_image)) {
+                unlink($blog->blog_image);
+            }
+
+            //update the blog post with the new data
+            $blog->update([
+                'blog_category_id' => $request->blog_category_id,
+                'blog_title' => $request->blog_title,
+                'blog_tags' => $request->blog_tags,
+                'blog_description' => $request->blog_description,
+                'blog_image' => $save_url,
+                'updated_at' => Carbon::now(),
+            ]);
+            $notification = array(
+                'message' => 'Blog Post Updated with Image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.blog')->with($notification);
+        } else {
+            $blog->update([
+                'blog_category_id' => $request->blog_category_id,
+                'blog_title' => $request->blog_title,
+                'blog_tags' => $request->blog_tags,
+                'blog_description' => $request->blog_description,
+                'updated_at' => Carbon::now(),
+            ]);
+            $notification = array(
+                'message' => 'Blog Post Updated without Image Successfully',
+                'alert-type' => 'success'
+            );
+            return redirect()->route('all.blog')->with($notification);
+        }
+    } // end method
+
+    public function DeleteBlog($id)
+    {
+        //delete image on localhost
+        $blog = Blog::findOrFail($id);
+        $img = $blog->blog_image;
+        unlink($img); //delete from local storage
+        //delete data by id in DB
+        Blog::findOrFail($id)->delete();
+        $notification = array(
+            'message' => 'Blog Post Deleted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
     } //end method
 }
